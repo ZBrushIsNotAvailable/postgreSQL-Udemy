cb5bcba54b60f9e3fc2983c8566e0e99a8b7ea52
Alex Medvediev <oleksandr.medvediev@starnavi.io>
2023-03-02 13:07:14 +0200

    first

--- insert-and-update-records.sql
+++ insert-and-update-records.sql
@@ -1,31 +1,36 @@
-create table customers  (
-    customer_id serial primary key ,
-    first_name varchar(50),
-    last_name varchar(50),
-    email varchar(150),
-    age int
-                          );
+create table customers
+(
+    customer_id serial primary key,
+    first_name  varchar(50),
+    last_name   varchar(50),
+    email       varchar(150),
+    age         int
+);
 
 /* insert data in specific columns and return specific record from modified row */
 insert into customers (first_name, last_name)
-values ('Mike O''konell', 'Jr.') RETURNING customer_id;
+values ('Mike O''konell', 'Jr.')
+RETURNING customer_id;
 
 insert into customers (first_name, last_name, email, age)
-values ('Alex', 'Med','sypchik@gmail.com',10);
+values ('Alex', 'Med', 'sypchik@gmail.com', 10);
 
 insert into customers (first_name)
-values ('o''konell') RETURNING *;
+values ('o''konell')
+RETURNING *;
 
 /* add new column */
-alter table  customers add is_enable varchar(2);
+alter table customers
+    add is_enable varchar(2);
 
 /* update all records */
-update customers set is_enable ='Y';
+update customers
+set is_enable ='Y';
 
 /* update specific record(-s) */
 update customers
 set first_name = 'Koons',
-    last_name = 'Soft'
+    last_name  = 'Soft'
 where customer_id = 4;
 
 /* update specific record(-s) and return modified rows */
